68c2f5f822300c9b33c4f6c3109f5c6e63966010
Bjorn Neergaard <bjorn@neersighted.com>
2014-08-02 00:52:10 -0500

    Version 0.1.1

--- nbnc.go
+++ nbnc.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	Name        string = "nbnc"
-	Version     string = "0.0.1"
+	Version     string = "0.1.1"
 	Description string = "simple null (transparent) bnc"
 
 	AuthTimeout  time.Duration = 15
